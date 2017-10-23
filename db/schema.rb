f2e263b38f69c0f977f67a3e62b5db14599eac2a
Dione Wilson <dionew1@gmail.com>
2017-10-23 15:45:24 -0600

    Favorites migration added

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023205523) do
+ActiveRecord::Schema.define(version: 20171023211555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "gif_id"
+    t.index ["gif_id"], name: "index_favorites_on_gif_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "gifs", force: :cascade do |t|
@@ -30,4 +37,6 @@ ActiveRecord::Schema.define(version: 20171023205523) do
     t.string "password_digest"
   end
 
+  add_foreign_key "favorites", "gifs"
+  add_foreign_key "favorites", "users"
 end
