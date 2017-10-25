9a570b705198db1876479e0ab287433fc9e58c69
Dione Wilson <dionew1@gmail.com>
2017-10-24 21:38:47 -0600

    add admin routes

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,7 +1,11 @@
+require 'net/http'
+require 'json'
+
 class Gif < ApplicationRecord
   validates :url, presence: true, uniqueness: true
-  
+
   belongs_to :category
   has_many :favorites
   has_many :users, through: :favorites
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  namespace :admin do
+    resources :categories, only: [:new, :create, :destroy, :index]
+    resources :gifs, only: [:new, :create, :destroy, :index]
+  end
 end
